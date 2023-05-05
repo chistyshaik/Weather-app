b60d2e28acdedec9d150ae0e25d14abf37a3f51a
Shaik Chisty <shaikchisty8@gmail.com>
2023-05-05 13:31:42 +0530

    html and css styling done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios'
 
-function App() {
+export default function App() {
+  const APIkey = '895284fb2d2c50a520ea537456963d9c' ;
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=895284fb2d2c50a520ea537456963d9c';
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="head ">
+          <div className="location">
+            <p>dallas</p>
+          </div>
+          <div className="temp">
+            <h1> 63°F </h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="foot">
+          <div className="feels">
+            <p className='bold'>65°F</p>
+            <p>Feels like</p>
+          </div>
+          <div className="humidity">
+            <p className='bold'>20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className='bold'>11 MPH</p>
+            <p>Wind speed</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
